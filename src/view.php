83eb1c135d7ebc007d58a1a330a9ea2bc45ea513
Erik Figueiredo <falecom@erikfigueiredo.com.br>
2014-10-17 07:15:22 -0300

    Inclusão de CSS

--- src/view.php
+++ src/view.php
@@ -3,6 +3,12 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Curso git - compartilhado</title>
+	<style>
+		body {
+			font-size: 14px;;
+			font-family: Verdana, Arial;
+		}
+	</style>
 </head>
 <body>
 	<h1>Curso git - compartilhado - teste de push online</h1>
